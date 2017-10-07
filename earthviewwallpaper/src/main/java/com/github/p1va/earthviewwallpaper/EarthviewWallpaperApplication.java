ed11ed6ee948a4e90fa22bf3c1f06de5a95a174d
p1va <stefano.piva9@gmail.com>
2017-10-07 14:38:44 +0200

    Removed unused assets

--- earthviewwallpaper/src/main/java/com/github/p1va/earthviewwallpaper/EarthviewWallpaperApplication.java
+++ earthviewwallpaper/src/main/java/com/github/p1va/earthviewwallpaper/EarthviewWallpaperApplication.java
@@ -2,12 +2,9 @@ package com.github.p1va.earthviewwallpaper;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
 import com.github.p1va.earthviewwallpaper.data.persistance.EarthViewImagesStore;
 import com.github.p1va.earthviewwallpaper.util.CacheMemoryUtils;
 import com.jakewharton.picasso.OkHttp3Downloader;
-import com.robotpajamas.stetho.couchbase.CouchbaseInspectorModulesProvider;
-import com.squareup.leakcanary.LeakCanary;
 import com.squareup.picasso.LruCache;
 import com.squareup.picasso.Picasso;
 
@@ -28,10 +25,10 @@ public class EarthviewWallpaperApplication extends Application {
         super.onCreate();
 
         // Setup Leak Canary
-        if(BuildConfig.DEBUG) {
-            if(LeakCanary.isInAnalyzerProcess(this)) return;
-            LeakCanary.install(this);
-        }
+        //if(BuildConfig.DEBUG) {
+        //    if(LeakCanary.isInAnalyzerProcess(this)) return;
+        //    LeakCanary.install(this);
+        //}
 
         // Setup Timber
         if(BuildConfig.DEBUG) {
@@ -44,14 +41,15 @@ public class EarthviewWallpaperApplication extends Application {
                 .downloader(new OkHttp3Downloader(this))
                 .build();
 
-        if(BuildConfig.DEBUG) {
+        //if(BuildConfig.DEBUG) {
             //picasso.setLoggingEnabled(true);
             //picasso.setIndicatorsEnabled(true);
-        }
+        //}
 
         Picasso.setSingletonInstance(picasso);
 
         // Setup Stetho
+        /*
         if(BuildConfig.DEBUG) {
             Stetho.initialize(
                     Stetho.newInitializerBuilder(this)
@@ -60,7 +58,7 @@ public class EarthviewWallpaperApplication extends Application {
                                     .showMetadata(true)
                                     .build())
                             .build());
-        }
+        }*/
 
         // Setup data layer
         EarthViewImagesStore.initialize(this);
